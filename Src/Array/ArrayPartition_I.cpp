74f6f42405400b9652c049908c8d5cf37bc474ca
JangoBoogaloo <jango.boogaloo@gmail.com>
2018-06-24 21:02:25 -0400

    Initial solution for #2. sort and pair minimum

--- Src/Array/ArrayPartition_I.cpp
+++ Src/Array/ArrayPartition_I.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "ArrayPartition_I.h"
-
+#include <algorithm>
 
 ArrayPartition_I::ArrayPartition_I()
 {
@@ -13,5 +13,11 @@ ArrayPartition_I::~ArrayPartition_I()
 
 int ArrayPartition_I::arrayPairSum(vector<int>& nums)
 {
-  return 0;
+  sort(nums.begin(), nums.end());
+  int total = 0;
+
+  for (int i = 0; i < nums.size(); i += 2) {
+    total += min(nums[i], nums[i+1]);
+  }
+  return total;
 }
