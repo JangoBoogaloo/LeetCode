2380798317d7645b1b6409e7674472f6434a52d2
JangoBoogaloo <jango.boogaloo@gmail.com>
2018-06-24 22:25:23 -0400

    use c++ function to reverse string. Fixed #5

--- Src/String/ReverseString.cpp
+++ Src/String/ReverseString.cpp
@@ -12,9 +12,6 @@ ReverseString::~ReverseString()
 
 string ReverseString::reverseString(string s)
 {
-  int size = s.size();
-  for (int i = 0; i < (size / 2); i++) {
-    swap(s[i], s[(size-1)-i]);
-  }
+  reverse(s.begin(), s.end());
   return s;
 }
